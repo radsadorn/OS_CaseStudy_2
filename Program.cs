ea5c6e761159d1a1cc5dd17355bdd18b9dd38ac5
radsadorn <sutthiratphutho@gmail.com>
2021-11-04 15:35:08 +0700

    Version 1

--- Program.cs
+++ Program.cs
@@ -10,43 +10,67 @@ namespace OS_Problem_02
         static int Back = 0;
         static int Count = 0;
 
+        private static object _Lock = new object();
+
         static void EnQueue(int eq)
         {
-            TSBuffer[Back] = eq;
-            Back++;
-            Back %= 10;
-            Count += 1;
+            lock (_Lock)
+            {
+                TSBuffer[Back] = eq;
+                Back++;
+                Back %= 10;
+                Count += 1;
+                Monitor.Pulse(_Lock);
+            }
         }
 
         static int DeQueue()
         {
-            int x = 0;
-            x = TSBuffer[Front];
-            Front++;
-            Front %= 10;
-            Count -= 1;
-            return x;
+            lock (_Lock)
+            {
+                int x = 0;
+                x = TSBuffer[Front];
+                Front++;
+                Front %= 10;
+                Count -= 1;
+                Monitor.Pulse(_Lock);
+                return x;
+            }
         }
 
         static void th01()
         {
             int i;
-
-            for (i = 1; i < 51; i++)
+            lock (_Lock)
             {
-                EnQueue(i);
-                Thread.Sleep(5);
+                for (i = 1; i < 51; i++)
+                {
+                    if (Count == 10)
+                    {
+                        Console.WriteLine("Wait th01");
+                        Monitor.Wait(_Lock);
+                    }
+                    EnQueue(i);
+                    Thread.Sleep(5);
+                }
             }
         }
 
         static void th011()
         {
             int i;
-
-            for (i = 100; i < 151; i++)
+            lock (_Lock)
             {
-                EnQueue(i);
-                Thread.Sleep(5);
+                for (i = 100; i < 151; i++)
+                {
+                    if (Count == 10)
+                    {
+                        Console.WriteLine("Wait th011");
+                        Monitor.Wait(_Lock);
+                    }
+                    EnQueue(i);
+                    Thread.Sleep(5);
+                }
             }
         }
 
@@ -56,23 +80,30 @@ namespace OS_Problem_02
             int i;
             int j;
 
-            for (i=0; i< 60; i++)
+            lock (_Lock)
             {
-                j = DeQueue();
-                Console.WriteLine("j={0}, thread:{1}", j, t);
-                Thread.Sleep(100);
+                for (i=0; i< 60; i++)
+                {
+                    if (Count == 0)
+                    {
+                        Monitor.Wait(_Lock);
+                    }
+                    j = DeQueue();
+                    Console.WriteLine("j={0}, thread:{1}", j, t);
+                    Thread.Sleep(100);
+                }
             }
         }
         static void Main(string[] args)
         {
             Thread t1 = new Thread(th01);
-            //Thread t11 = new Thread(th011);
+            Thread t11 = new Thread(th011);
             Thread t2 = new Thread(th02);
             //Thread t21 = new Thread(th02);
             //Thread t22 = new Thread(th02);
 
             t1.Start();
-            //t11.Start();
+            t11.Start();
             t2.Start(1);
             //t21.Start(2);
             //t22.Start(3);
